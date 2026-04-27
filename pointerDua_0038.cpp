10bd20cc69156578460942d78f192fdf4d0b6679
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-27 12:10:36 +0700

    menambahkan implementasi fungsi main untuk pengujian alokasi dinamis pada class 'mahasiswa'

--- pointerDua_0038.cpp
+++ pointerDua_0038.cpp
@@ -11,3 +11,13 @@ public:
     }
 };
 
+int main()
+{
+    // Pointer Object mhs menggunakan alokasi dinamis
+    mahasiswa *mhs = new mahasiswa; 
+    mhs->nim = 2;
+    mhs->showNim();
+    
+    delete mhs; // Menghapus memori yang dialokasikan
+    return 0;
+}
